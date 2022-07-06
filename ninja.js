9cb97d7534d849e526595b17c54d1af5dfe07bc1
sjperez1 <sarahpz479@gmail.com>
2022-07-05 18:55:53 -0600

    initialize repo

---
+++ ninja.js
@@ -0,0 +1,23 @@
+class Ninja {
+    constructor(name, health = 200){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName(){
+        console.log(`Name: ${this.name}`);
+        return this;
+    }
+    showStats(){
+        console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
+        return this;
+    }
+    drinkSake(){
+        this.strength +=10;
+        return this;
+    }
+}
+
+const spNinja = new Ninja("Sarah");
+spNinja.sayName().showStats().drinkSake();
